c3223587ae185ec81c9764336b32f63a47cc2425
LampMoth <ryanmeiring@outlook.com>
2023-06-26 14:07:49 +1200

    Fixed Titles and Page links, added comments

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
-const galleryLink = document.getElementById('gallerylink');
-const page2 = document.getElementById('page2');
+  const galleryLink = document.getElementById('gallerylink');
+  const page2 = document.getElementById('page2');
 
-galleryLink.addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent the default link behavior
+  galleryLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
 
-  const page2Bottom = page2.offsetTop + page2.offsetHeight - window.innerHeight;
-  window.scrollTo({ top: page2Bottom, behavior: 'smooth' });
-});
+    const page2Bottom = page2.offsetTop + page2.offsetHeight - window.innerHeight;
+    window.scrollTo({ top: page2Bottom, behavior: 'smooth' });
+  });
+
+
+  document.getElementById('contactlink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+
+    const page3 = document.querySelector('.Page3');
+    const page3Top = page3.offsetTop;
+    window.scrollTo({ top: page3Top, behavior: 'smooth' });
+  });
+
+
+
